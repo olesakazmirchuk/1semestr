e2b9b948d918aa32b1804339d9a6e7fab42b52a4
Olesia Kazmirchuk <xolesya049xx@gmail.com>
2026-09-06 18:56:19 +0300

    lab1

---
+++ lab1.py
@@ -0,0 +1,23 @@
+print ('Hello World!')
+o = 10
+m = 1.52
+k = 'Python'
+c = True
+w = False
+g = [1, 2, 3]
+print (o, type(o))
+print (m, type(m))
+print (k, type(k))
+print (c, type(c))
+print (w, type(w))
+print (g, type(g))
+
+p = 6
+q= 2
+print (p + q)
+print (p - q)
+print (p * q)
+print (p / q)
+print (p // q)
+print (p % q)
+print (p ** q)
