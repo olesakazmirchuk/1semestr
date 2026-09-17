f108c23cfaa5901c965d73204f68f44a78a51e59
Olesia Kazmirchuk <xolesya049xx@gmail.com>
2026-09-17 16:23:36 +0300

    lab1

--- lab1.py
+++ lab1.py
@@ -5,15 +5,24 @@ k = 'Python'
 c = True
 w = False
 g = [1, 2, 3]
+t = (4, 7, 9)
+d = {'name': 'Olesia', 'age': 16}
+st = {4, 7, 9}
+
 print (o, type(o))
 print (m, type(m))
 print (k, type(k))
 print (c, type(c))
 print (w, type(w))
 print (g, type(g))
+print (t, type(t))
+print (d, type(d))
+print (st, type(st))
 
-p = 6
-q= 2
+
+p = int(input('Enter p: '))
+q = int(input('Enter q: '))
+
 print (p + q)
 print (p - q)
 print (p * q)
